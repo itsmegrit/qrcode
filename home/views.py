e5c1e7e60dc0fbf161e1ba428195f2e9e782ece3
Phong <ngt.phongg@gmail.com>
2023-04-21 16:18:48 +0700

    test connect mongodb

--- home/views.py
+++ home/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import pymongo
 # Create your views here.
 
 
 def index(request):
     return render(request, 'pages/home.html')
+
+
+client = pymongo.MongoClient(
+    'mongodb+srv://ngtphongg:25251325@cluster0.yrdmc9z.mongodb.net/test')
+# Define DB Name
+dbname = client['managediemdanh']
+
+# Define Collection
+collection = dbname['sinhvien']
+
+sinhvien = collection.find({})
+
+for r in sinhvien:
+    print(r['tensinhvien'])
